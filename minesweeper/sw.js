50d9965ec5b066991b6cea4c52a73d8bd69a77b7
itonis <itonis@users.noreply.github.com>
2025-11-30 11:05:44 +0000

    deploy: 4c64a4f4bce5998ce0ffd07d87dafaacafc6767c

---
+++ minesweeper/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'minesweeper-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './icon.png',
+    './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
