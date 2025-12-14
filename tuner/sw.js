103ea85d52aa33173844714444f6fa7a81c12adb
itonis <madcrazy333@gmail.com>
2025-12-15 00:27:34 +0800

    [Tuner] #4 prompt
      Prompt
      ```
      Add sw.js, manifest and a icon to make this "installable" as a PWA
      ```

---
+++ tuner/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'tuner-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './main.js',
+    './audio.js',
+    './manifest.json',
+    './icon.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
